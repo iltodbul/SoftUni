901f04116e0c9ae7322ac560e714a8d00dccca9e
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2019-12-11 16:11:04 +0200

    Update Program.cs

--- C# Fundamentals/Mid Exam - 30 June 2019 Group 1/Contact List/Program.cs
+++ C# Fundamentals/Mid Exam - 30 June 2019 Group 1/Contact List/Program.cs
@@ -8,14 +8,14 @@ namespace Contact_List
         static void Main(string[] args)
         {
             var contacts = Console.ReadLine().Split().ToList();
-            var command = Console.ReadLine().Split().ToList();
+            var commands = Console.ReadLine().Split().ToList();
 
-            while (command[0] != "Print")
+            while (commands[0] != "Print")
             {
-                if (command[0] == "Add")
+                if (commands[0] == "Add")
                 {
-                    string contact = command[1];
-                    int index = int.Parse(command[2]);
+                    string contact = commands[1];
+                    int index = int.Parse(commands[2]);
                     if (index >= 0 && index < contacts.Count)
                     {
                         if (contacts.Contains(contact))
@@ -29,33 +29,33 @@ namespace Contact_List
                     }
 
                 }
-                else if (command[0] == "Remove")
+                else if (commands[0] == "Remove")
                 {
-                    int index = int.Parse(command[1]);
+                    int index = int.Parse(commands[1]);
                     if (index >= 0 && index < contacts.Count)
                     {
                         contacts.RemoveAt(index);
                     }
                 }
-                else if (command[0] == "Export")
+                else if (commands[0] == "Export")
                 {
-                    int startIndex = int.Parse(command[1]);
-                    int count = int.Parse(command[2]);
+                    int startIndex = int.Parse(commands[1]);
+                    int count = int.Parse(commands[2]);
                     if (startIndex >= 0 && startIndex < contacts.Count - 1)
                     {
                         var print = contacts.Skip(startIndex).Take(count).ToList();
                         Console.WriteLine(string.Join(" ", print));
                     }
                 }
-                command = Console.ReadLine().Split().ToList();
+                commands = Console.ReadLine().Split().ToList();
             }
-            if (command[0] == "Print")
+            if (commands[0] == "Print")
             {
-                if (command[1]== "Normal")
+                if (commands[1]== "Normal")
                 {
                     Console.WriteLine($"Contacts: {string.Join(" ",contacts)}");
                 }
-                else if (command[1]== "Reversed")
+                else if (commands[1]== "Reversed")
                 {
                     contacts.Reverse();
                     Console.WriteLine($"Contacts: {string.Join(" ", contacts)}");
